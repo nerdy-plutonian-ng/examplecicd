45e06ab6e22f732db82dfc1dc341f45f5cf1bc52
nerdy-plutonian-ng <nerdymykl@gmail.com>
2024-01-18 15:08:59 +0000

    added smiley to greeting

--- app/src/main/java/com/plutoapps/examplecicd/MainActivity.kt
+++ app/src/main/java/com/plutoapps/examplecicd/MainActivity.kt
@@ -25,7 +25,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("CI/CD")
+                    Greeting("CI/CD :)")
                 }
             }
         }
